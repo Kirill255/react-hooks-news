4bb9afffcf886811b1a999d3d4a2b3ddbd345ad7
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-07-25 19:26:22 +0300

    add base router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+
+import CreateLink from "./components/CreateLink";
+import Login from "./components/Login";
+import ForgotPassword from "./components/ForgotPassword";
+import Search from "./components/Search";
+import LinkList from "./components/LinkList";
+import LinkDetail from "./components/LinkDetail";
 
 const App = () => (
-  <div style={{ margin: "0 auto", textAlign: "center" }}>
-    <img style={{ height: "100vh" }} src="/logo.png" alt="Logo" />
-  </div>
+  <Router>
+    <Switch>
+      <Route path="/" exact render={() => <Redirect to="/new/1" />} />
+      <Route path="/create" component={CreateLink} />
+      <Route path="/login" component={Login} />
+      <Route path="/forgot" component={ForgotPassword} />
+      <Route path="/search" component={Search} />
+      <Route path="/top" component={LinkList} />
+      <Route path="/new/:page" component={LinkList} />
+      <Route path="/link/:linkId" component={LinkDetail} />
+    </Switch>
+  </Router>
 );
 
 export default App;
