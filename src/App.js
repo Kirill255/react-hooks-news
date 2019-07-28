578bb9d83f8b15cb70f455b0294b1346906398f4
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-07-28 22:40:40 +0300

    create FirebaseContext context

--- src/App.js
+++ src/App.js
@@ -9,24 +9,36 @@ import Search from "./components/Search";
 import LinkList from "./components/LinkList";
 import LinkDetail from "./components/LinkDetail";
 
-const App = () => (
-  <Router>
-    <div className="app-container">
-      <Header />
-      <div className="route-container">
-        <Switch>
-          <Route path="/" exact render={() => <Redirect to="/new/1" />} />
-          <Route path="/create" component={CreateLink} />
-          <Route path="/login" component={Login} />
-          <Route path="/forgot" component={ForgotPassword} />
-          <Route path="/search" component={Search} />
-          <Route path="/top" component={LinkList} />
-          <Route path="/new/:page" component={LinkList} />
-          <Route path="/link/:linkId" component={LinkDetail} />
-        </Switch>
-      </div>
-    </div>
-  </Router>
-);
+import useAuth from "./hooks/useAuth";
+import firebase, { FirebaseContext } from "./firebase";
+
+const App = () => {
+  const user = useAuth();
+  if (user) {
+    console.log(user.toJSON()); // firebase метод объекта user https://firebase.google.com/docs/reference/js/firebase.User.html
+  }
+
+  return (
+    <Router>
+      <FirebaseContext.Provider value={{ user, firebase }}>
+        <div className="app-container">
+          <Header />
+          <div className="route-container">
+            <Switch>
+              <Route path="/" exact render={() => <Redirect to="/new/1" />} />
+              <Route path="/create" component={CreateLink} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot" component={ForgotPassword} />
+              <Route path="/search" component={Search} />
+              <Route path="/top" component={LinkList} />
+              <Route path="/new/:page" component={LinkList} />
+              <Route path="/link/:linkId" component={LinkDetail} />
+            </Switch>
+          </div>
+        </div>
+      </FirebaseContext.Provider>
+    </Router>
+  );
+};
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,49 @@
-import React from "react";
+import React, { useContext } from "react";
 import { withRouter, NavLink } from "react-router-dom";
 
-const Header = () => (
-  <div className="header">
-    <div className="flex">
-      <img src="/logo.png" alt="Hooks News" className="logo" />
-      <NavLink to="/" className="header-title">
-        Hooks News
-      </NavLink>
-      <NavLink to="/" className="header-link">
-        new
-      </NavLink>
-      <div className="divider">|</div>
-      <NavLink to="/top" className="header-link">
-        top
-      </NavLink>
-      <div className="divider">|</div>
-      <NavLink to="/search" className="header-link">
-        search
-      </NavLink>
-      <div className="divider">|</div>
-      <NavLink to="/create" className="header-link">
-        submit
-      </NavLink>
+import { FirebaseContext } from "../firebase";
+
+const Header = () => {
+  const { user, firebase } = useContext(FirebaseContext);
+
+  return (
+    <div className="header">
+      <div className="flex">
+        <img src="/logo.png" alt="Hooks News" className="logo" />
+        <NavLink to="/" className="header-title">
+          Hooks News
+        </NavLink>
+        <NavLink to="/" className="header-link">
+          new
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/top" className="header-link">
+          top
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/search" className="header-link">
+          search
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/create" className="header-link">
+          submit
+        </NavLink>
+      </div>
+      <div className="flex">
+        {user ? (
+          <>
+            <div className="header-name">{user.displayName}</div>
+            <div className="divider">|</div>
+            <div className="header-button">logout</div>
+          </>
+        ) : (
+          <NavLink to="/login" className="header-link">
+            login
+          </NavLink>
+        )}
+      </div>
     </div>
-    <div className="flex">
-      <NavLink to="/login" className="header-link">
-        login
-      </NavLink>
-    </div>
-  </div>
-);
+  );
+};
 
 export default withRouter(Header);
