b8b7c1bcaeb65642118d129998c33489c44a6477
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-07-30 00:39:37 +0300

    add createlink and add validateCreateLink utils

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,7 +1,53 @@
 import React from "react";
 
+import useFormValidation from "../hooks/useFormValidation";
+import validateCreateLink from "../utils/validateCreateLink";
+
+const INITIAL_STATE = {
+  description: "",
+  url: ""
+};
+
 const CreateLink = () => {
-  return <div>CreateLink</div>;
+  const { values, errors, handleChange, handleBlur, handleSubmit } = useFormValidation(
+    INITIAL_STATE,
+    validateCreateLink,
+    handleCreateLink
+  );
+
+  function handleCreateLink() {
+    console.log("Link created!");
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-column mt3">
+      <input
+        className={errors.description && "error-input"}
+        type="text"
+        placeholder="A description for your link"
+        autoComplete="off"
+        name="description"
+        value={values.description}
+        onBlur={handleBlur}
+        onChange={handleChange}
+      />
+      {errors.description && <p className="error-text">{errors.description}</p>}
+      <input
+        className={errors.url && "error-input"}
+        type="text"
+        placeholder="The URL for the link"
+        autoComplete="off"
+        name="url"
+        value={values.url}
+        onBlur={handleBlur}
+        onChange={handleChange}
+      />
+      {errors.url && <p className="error-text">{errors.url}</p>}
+      <button className="button" type="submit">
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default CreateLink;
