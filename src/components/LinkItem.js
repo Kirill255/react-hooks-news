59713f33a5ff9229397f2232f395bf1a5c1de50b
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-07-30 18:18:05 +0300

    fetch and display links

---
+++ src/components/LinkItem.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const LinkItem = ({ link, showCount, index }) => {
+  return (
+    <div className="flex items-start mt2">
+      <div className="flex items-center">
+        {showCount && <span className="gray">{index}.</span>}
+        <div className="vote-button">▲</div>
+      </div>
+      <div className="ml1">
+        <div>
+          {link.description} <span className="link">({link.url})</span>
+        </div>
+        <div className="f6 lh-copy gray">
+          {link.votes.length} votes by {link.postedBy.name} {link.created}
+          {" | "}
+          <Link to={`/link/${link.id}`}>
+            {link.comments.length > 0 ? `${link.comments.length} comments` : "discuss"}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LinkItem;
--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+
+import LinkItem from "./LinkItem";
+
+import { FirebaseContext } from "../firebase";
 
 const LinkList = () => {
-  return <div>LinkList</div>;
+  const [links, setLinks] = useState([]);
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const unsubscribe = firebase.db.collection("links").onSnapshot((docsSnapshot) => {
+      console.log(docsSnapshot);
+      if (docsSnapshot.empty) {
+        console.log("No matching documents.");
+        return;
+      }
+      const linksFromDB = docsSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })); // return a new object with the added id
+      setLinks(linksFromDB);
+    });
+
+    return () => unsubscribe();
+    // https://github.com/facebook/react/issues/14920
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div>
+      {links.map((link, index) => (
+        <LinkItem key={link.id} link={link} showCount={true} index={index + 1} />
+      ))}
+    </div>
+  );
 };
 
 export default LinkList;
