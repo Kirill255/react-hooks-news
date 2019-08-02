2f0112e904fed1ea73cd8e5974485ad3838d5943
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-08-03 00:32:32 +0300

    add cursor-based pagination with startAfter and limit

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -3,12 +3,16 @@ import React, { useState, useEffect, useContext } from "react";
 import LinkItem from "./LinkItem";
 
 import { FirebaseContext } from "../firebase";
+import { LINKS_PER_PAGE } from "../utils";
 
-// location from router
-const LinkList = ({ location }) => {
+// location, match, history from router
+const LinkList = ({ location, match, history }) => {
   const [links, setLinks] = useState([]);
+  const [cursor, setCursor] = useState(null);
   const { firebase } = useContext(FirebaseContext);
+  const isNewPage = location.pathname.includes("new");
   const isTopPage = location.pathname.includes("top");
+  const page = Number(match.params.page);
 
   useEffect(() => {
     const unsubscribe = getLinks();
@@ -16,15 +20,20 @@ const LinkList = ({ location }) => {
     return () => unsubscribe();
     // https://github.com/facebook/react/issues/14920
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isTopPage]);
+  }, [isTopPage, page]);
 
   function getLinks() {
+    const hasCursor = Boolean(cursor);
     if (isTopPage) {
       // prettier-ignore
-      return firebase.db.collection("links").orderBy("voteCount", "desc").onSnapshot(handleSnapshot);
+      return firebase.db.collection("links").orderBy("voteCount", "desc").limit(LINKS_PER_PAGE).onSnapshot(handleSnapshot);
+    } else if (page === 1) {
+      // prettier-ignore
+      return firebase.db.collection("links").orderBy("created", "desc").limit(LINKS_PER_PAGE).onSnapshot(handleSnapshot);
+    } else if (hasCursor) {
+      // prettier-ignore
+      return firebase.db.collection("links").orderBy("created", "desc").startAfter(cursor.created).limit(LINKS_PER_PAGE).onSnapshot(handleSnapshot);
     }
-    // prettier-ignore
-    return firebase.db.collection("links").orderBy("created", "desc").onSnapshot(handleSnapshot);
   }
 
   function handleSnapshot(docsSnapshot) {
@@ -34,16 +43,49 @@ const LinkList = ({ location }) => {
       return;
     }
     const linksFromDB = docsSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })); // return a new object with the added id
+    const lastLink = linksFromDB[linksFromDB.length - 1];
     setLinks(linksFromDB);
+    setCursor(lastLink);
   }
+
+  const visitPreviousPage = () => {
+    if (page > 1) {
+      history.push(`/new/${page - 1}`);
+    }
+  };
+
+  const visitNextPage = () => {
+    // if page <= total pages
+    if (page <= Math.ceil(links.length / LINKS_PER_PAGE)) {
+      history.push(`/new/${page + 1}`);
+    }
+  };
+
+  // const pageIndex = page ? (page - 1) * LINKS_PER_PAGE + 1 : 0;
 
   return (
     <div>
       {links.map((link, index) => (
         <LinkItem key={link.id} link={link} showCount={true} index={index + 1} />
       ))}
+      {isNewPage && (
+        <div className="pagination">
+          <div className="pointer mr2" onClick={visitPreviousPage}>
+            Previous
+          </div>
+          <div className="pointer" onClick={visitNextPage}>
+            Next
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
 export default LinkList;
+
+/*
+const pageIndex = page ? (page - 1) * LINKS_PER_PAGE + 1 : 0;
+
+<LinkItem key={link.id} link={link} showCount={true} index={index + pageIndex} />
+*/
